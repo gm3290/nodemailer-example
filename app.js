2d534218d67bdf5e5f6d4371a43fac37d6bee6e8
topmember1929 <clearg1929@gmail.com>
2018-06-13 19:02:59 +0200

    config and send email

---
+++ app.js
@@ -0,0 +1,40 @@
+var nodemailer = require('nodemailer');
+const _ = require('underscore');
+const fs = require('fs');
+
+const transport = {
+  host: 'smtp.office365.com',
+  port: '587',
+  auth: { user: 'your account', pass: 'your password' },
+  secureConnection: false,
+  tls: { ciphers: 'SSLv3' }
+}
+const from = "your account";
+
+const sendMailer = (to, subject, template) => {
+
+  var transporter = nodemailer.createTransport(transport);
+  
+  const mailOptions = {
+    from: from,
+    to: to, subject: subject,
+    html: "<h1>Test email </h1>"
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err)
+      console.log(err)
+    else
+      console.log(info);
+  });
+}
+
+const start = async () => {
+  try {
+    sendMailer("to", "Test email", "template")
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+start()
